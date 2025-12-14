d4fa77599e3dd4438453ea165bf69d37c384b009
ndlovuai100-ai <ndlovuai100@gmail.com>
2025-12-15 00:00:35 +0200

    Add core.py to ndlovu_core directory

---
+++ ndlovu_core/core.py
@@ -0,0 +1,19 @@
+ndlovu-ai/
+│
+├── ndlovu_core/
+│   ├── __init__.py
+│   ├── core.py
+│
+├── nkhudu_code/
+│   ├── __init__.py
+│
+├── project_codes/
+│   ├── __init__.py
+│
+├── oracode/
+│   ├── __init__.py
+│
+├── data/
+│   └── local_state.json
+│
+├── cli.py
